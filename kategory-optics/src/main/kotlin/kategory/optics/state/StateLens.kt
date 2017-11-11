27edbf4ee436289d93885d973433e09ad1d1c5a7
Simon Vergauwen <vergauwen.simon@gmail.com>
2017-11-10 20:44:34 -0500

    IndexedState and State must be same data type

--- kategory-optics/src/main/kotlin/kategory/optics/state/StateLens.kt
+++ kategory-optics/src/main/kotlin/kategory/optics/state/StateLens.kt
@@ -4,23 +4,39 @@ import kategory.*
 import kategory.optics.Lens
 import kategory.optics.PLens
 
+/**
+ * Transforms a PLens into a State
+ */
 fun <S, T, A, B> PLens<S, T, A, B>.toState() = State<S, A> { s ->
     s toT get(s)
 }
 
+/**
+ * Extracts the value viewed through the lens
+ */
 fun <S, T, A, B> PLens<S, T, A, B>.extract() = toState()
 
+/**
+ * Extracts the value viewed through the lens and applies [f] over it
+ */
 inline fun <S, T, A, B, C> PLens<S, T, A, B>.extracts(crossinline f: (A) -> C) =
         extract().ev().map { f(it) }.ev()
 
-fun <S, A> Lens<S, A>.mod(f: (A) -> A) = State<S, A> {
-    val a = get(it)
+/**
+ * Modify the value viewed through the lens and returns its *new* value
+ */
+fun <S, T, A, B> PLens<S, T, A, B>.mod(f: (A) -> B): IndexedStateT<IdHK, S, T, B> = IndexedStateT(Id.applicative()) { s ->
+    val a = get(s)
     val b = f(a)
-    set(it, b) toT b
+    Id.pure(set(s, b) toT b)
 }
 
+/** modify the value viewed through the lens and returns its *old* value */
+//def modo(f: A => B): IndexedState[S, T, A] =toState.leftMap(lens.modify(f))
+fun <S, T, A, B> PLens<S, T, A, B>.modo(f: (A) -> B): IndexedStateT<IdHK, S, T, A> =
 fun <S, A> Lens<S, A>.mod_(f: (A) -> A) = State<S, Unit> {
     val a = get(it)
     val b = f(a)
     set(it, b) toT Unit
 }
+
--- kategory-optics/src/main/kotlin/kategory/optics/state/StateOptional.kt
+++ kategory-optics/src/main/kotlin/kategory/optics/state/StateOptional.kt
@@ -5,19 +5,55 @@ import kategory.optics.Optional
 import kategory.optics.POptional
 import kategory.optics.modify
 
+/**
+ * Transforms a POptional into a State
+ */
 fun <S, T, A, B> POptional<S, T, A, B>.toState() = State<S, Option<A>> { s ->
     s toT getOption(s)
 }
 
+/**
+ * Extracts the value viewed through the optional
+ */
 fun <S, T, A, B> POptional<S, T, A, B>.extract(): StateT<IdHK, S, Option<A>> = toState()
 
+/**
+ * Extracts the value viewed through the optional and applies [f] over it
+ */
 inline fun <S, T, A, B> POptional<S, T, A, B>.extracts(crossinline f: (A) -> B): StateT<IdHK, S, Option<B>> =
         extract().ev().map { it.map(f) }.ev()
 
-fun <S, A> Optional<S, A>.mod(f: (A) -> A) = State<S, Option<A>> {
-    modify(it, f) toT getOption(it)
+/**
+ * Modify the value viewed through the Optional and return its *new* value, if there is one
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.mod(f: (A) -> B): IndexedStateT<IdHK, S, T, Option<B>> =
+        modo(f).map { it.map(f) }.ev()
+
+/**
+ * Modify the value viewed through the Optional and return its *old* value, if there was one
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.modo(f: (A) -> B): IndexedStateT<IdHK, S, T, Option<A>> = IndexedStateT(Id.applicative()) { s ->
+    Id.pure(modify(s, f) toT getOption(s))
 }
 
-fun <S, A> Optional<S, A>.mod_(f: (A) -> A) = State<S, Unit> {
-    modify(it, f) toT Unit
+/**
+ * Modify the value viewed through the Optional and ignores both values
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.mod_(f: (A) -> B): IndexedStateT<IdHK, S, T, Unit> = IndexedStateT(Id.applicative()) { s ->
+    Id.pure(modify(s, f) toT Unit)
 }
+
+/**
+ * Set the value viewed through the Optional and returns its *new* value
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.assign(b: B): IndexedStateT<IdHK, S, T, Option<B>> = mod { _ -> b }
+
+/**
+ * Set the value viewed through the Optional and return its *old* value, if there was one
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.assigno(b: B): IndexedStateT<IdHK, S, T, Option<A>> = modo { _ -> b }
+
+/**
+ * Set the value viewed through the Optional and ignores both values
+ */
+fun <S, T, A, B> POptional<S, T, A, B>.assign_(b: B): IndexedStateT<IdHK, S, T, Unit> = mod_{ _ -> b }
